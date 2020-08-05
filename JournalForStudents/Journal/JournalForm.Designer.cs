669e5b745cfe7b41f5fc37a8170744246b2d6799
DenJamIN <denlavre@mail.ru>
2020-08-05 16:05:46 +0300

    Upload DataGridView

--- JournalForStudents/Journal/JournalForm.Designer.cs
+++ JournalForStudents/Journal/JournalForm.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.tableStudent = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tableStudent)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -50,17 +55,55 @@
             this.panel2.Size = new System.Drawing.Size(1273, 57);
             this.panel2.TabIndex = 1;
             // 
+            // tableStudent
+            // 
+            this.tableStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.groupName,
+            this.Column1});
+            this.tableStudent.Location = new System.Drawing.Point(167, 194);
+            this.tableStudent.Name = "tableStudent";
+            this.tableStudent.RowHeadersWidth = 51;
+            this.tableStudent.RowTemplate.Height = 24;
+            this.tableStudent.Size = new System.Drawing.Size(1273, 606);
+            this.tableStudent.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Maroon;
+            this.panel3.Location = new System.Drawing.Point(167, 55);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(616, 139);
+            this.panel3.TabIndex = 3;
+            // 
+            // groupName
+            // 
+            this.groupName.HeaderText = "Список студентов";
+            this.groupName.MinimumWidth = 6;
+            this.groupName.Name = "groupName";
+            this.groupName.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
             // JournalForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1440, 800);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.tableStudent);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "JournalForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tableStudent)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +112,10 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView tableStudent;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn groupName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column1;
     }
 }
 
